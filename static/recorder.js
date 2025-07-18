6d82c1d28b83972809d1ac00987c82469fe02174
Ryan Marshall <ryanmarshall@Ryans-MacBook-Pro.local>
2025-07-17 22:28:32 -0400

    Some new additions

--- static/recorder.js
+++ static/recorder.js
@@ -11,14 +11,17 @@ document.onload = console.log("Recorder script loaded");
 const mic_button = document.getElementById("jarvis-button");
 const playback = document.getElementById("audio");
 const jarvisBlock = document.getElementById("response");
+const userBlock = document.getElementById("userWords");
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 //adjust values
 const r  = new SpeechRecognition();
+r.interimResults = true;
 
 let isRecording = false;
 let userWords;
+let userArray = [];
 
 //I need to use await and async to make it so the recognizer finshes before sending the data to the python server
 //PROCESS:
@@ -31,14 +34,30 @@ function captureEvent(audioEvent){
     console.log("User said: " + userWords);
 }
 
+function captureEventWithInterim(audioEvent){
+    console.log("Recording started");
+    let chunk = audioEvent.results[0][0].transcript
+    userBlock.textContent = chunk; //fix this this doesnt work... yet >:)
+    userArray.push(chunk);
+}
+
 function recordAndSend(){
+    console.log("button pressed");
     if(!isRecording){
+        mic_button.classList.add("is_recording");
+        //console.log("Recording");
         isRecording = true;
         jarvisBlock.textContent = "Jarvis is listening...";
         r.start();
+        mic_button.value = "Recording...";
         r.addEventListener('result', captureEvent);
+        //userArray.join(" ");
+        //userWords = userArray;
     }
-    else{
+    else if(userWords !== undefined && userWords.legnth > 0){
+        //console.log("else if");
+        mic_button.disabled = true;
+        mic_button.classList.remove("is_recording");
         r.removeEventListener('result', captureEvent); 
         r.stop();
         isRecording = false;
@@ -46,18 +65,35 @@ function recordAndSend(){
         let jsonData = JSON.stringify(userWords);
         console.log("Data to send: " + jsonData);
         jarvisBlock.textContent = "Jarvis is thinking...";
-        $.ajax({
-            url: '/jarvis',
-            type: 'POST',
-            contentType: 'application/json',
-            dataType: 'json',
-            data: jsonData,
-            success: function(result){
-                console.log("Data received back");
-                jarvisBlock.textContent = "Jarvis says: " + result;
-            }
-        });
+        try {
+            $.ajax({
+                url: '/jarvis',
+                type: 'POST',
+                contentType: 'application/json',
+                dataType: 'json',
+                data: jsonData,
+                success: function(result){
+                    console.log("Data received back");
+                    jarvisBlock.textContent = "Jarvis says: " + result;
+                    mic_button.disabled = false;
+                    mic_button.value = "Click me!";
+                }
+            });
+        }
+        catch(error){
+            console.log("Something went wrong");
+            jarvisBlock.textContent = "Jarvis encountered an error :(";
+        }
         console.log("Data sent to server");
+        userArray = [];
+    }
+    else{
+        //console.log("Else")
+        r.stop();
+        isRecording = false;
+        jarvisBlock.textContent = "Jarvis couldn't here you :(" + "\n Could you please try again?";
+        mic_button.classList.remove("is_recording");
+        mic_button.value = "Click me!";
     }
 }
 
