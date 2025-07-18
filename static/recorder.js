def005727dd1bc8fa975f48aca845563b44c50fa
Ryan Marshall <ryanmarshall@Ryans-MacBook-Pro.local>
2025-07-17 21:10:07 -0400

    button kind of works here

--- static/recorder.js
+++ static/recorder.js
@@ -18,28 +18,33 @@ const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogni
 const r  = new SpeechRecognition();
 
 let isRecording = false;
-
 let userWords;
 
 //I need to use await and async to make it so the recognizer finshes before sending the data to the python server
 //PROCESS:
 //Click button => start recording => wait for user to click button again => analyze speech => send data to server => 
-//wait for response => update html to dirsplay response
-mic_button.addEventListener("click", () => {
+//wait for response => update html to display response
+
+function captureEvent(audioEvent){
+    console.log("Recording started");
+    userWords = audioEvent.results[0][0].transcript //this part takes a variable amount of time, so the stop button function needs to wait for this to finish
+    console.log("User said: " + userWords);
+}
+
+function recordAndSend(){
     if(!isRecording){
         isRecording = true;
+        jarvisBlock.textContent = "Jarvis is listening...";
         r.start();
-        r.addEventListener('result', (e) => {
-            userWords = e.results[0][0].transcript //this part takes a variable amount of time, so the stop button function needs to wait for this to finish
-            console.log("User said: " + userWords);
-        });
+        r.addEventListener('result', captureEvent);
     }
     else{
+        r.removeEventListener('result', captureEvent); 
         r.stop();
         isRecording = false;
         //this part below needs to wait for the recognizer
-        jsonData = JSON.stringify(userWords);
-        console.log("Data to send:" + jsonData);
+        let jsonData = JSON.stringify(userWords);
+        console.log("Data to send: " + jsonData);
         jarvisBlock.textContent = "Jarvis is thinking...";
         $.ajax({
             url: '/jarvis',
@@ -51,10 +56,10 @@ mic_button.addEventListener("click", () => {
                 console.log("Data received back");
                 jarvisBlock.textContent = "Jarvis says: " + result;
             }
-        })
+        });
         console.log("Data sent to server");
-        }
-    });
+    }
+}
 
-
+mic_button.addEventListener("click", recordAndSend);
 
